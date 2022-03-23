139495129619358ee6d3cbd399f1b448ac292f1d
padmakant <padmakant.b@gmail.com>
2022-03-23 13:54:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { NavigationMenu } from "./Components/NavigationMenu/NavigationMenu";
+
+function App() {
+  return <NavigationMenu />;
+}
+
+export default App;
---
+++ src/Components/NavigationMenu/NavigationMenu.js
@@ -0,0 +1,7 @@
+import React from "react";
+import css from "./NavigationMenu.module.scss";
+
+export const NavigationMenu = () => {
+  console.log("Nav bar");
+  return <div className={css.abc}>This is navigation</div>;
+};
