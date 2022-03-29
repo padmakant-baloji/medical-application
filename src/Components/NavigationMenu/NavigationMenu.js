679829eb1cd4c231d3ebd32320f156310efdd1fa
prajwal_b <prajwalwebdev07@gmail.com>
2022-03-29 21:51:25 +0530

    git setup check

--- src/Components/NavigationMenu/NavigationMenu.js
+++ src/Components/NavigationMenu/NavigationMenu.js
@@ -3,5 +3,6 @@ import css from "./NavigationMenu.module.scss";
 
 export const NavigationMenu = () => {
   console.log("Nav bar");
+  console.log('git set up check');
   return <div className={css.abc}>This is navigation</div>;
 };
